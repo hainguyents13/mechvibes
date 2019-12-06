34406c423cf56fa04ac3c31c18ac375d6be0a757
Hải Nguyễn <hainguyen.ts13@gmail.com>
2019-12-07 01:03:43 +0700

    first source commit

---
+++ src/main.js
@@ -0,0 +1,62 @@
+// Modules to control application life and create native browser window
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let win;
+
+function createWindow() {
+  // Create the browser window.
+  win = new BrowserWindow({
+    width: 400,
+    height: 600,
+    webSecurity: false,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: false,
+      nodeIntegration: true,
+    },
+  });
+
+  // and load the index.html of the app.
+  win.loadFile('./src/index.html');
+
+  // Open the DevTools.
+  // win.webContents.openDevTools();
+
+  // Emitted when the window is closed.
+  win.on('closed', function() {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    win = null;
+  });
+}
+
+app.commandLine.appendSwitch('autoplay-policy', 'no-user-gesture-required');
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow);
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function() {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', function() {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (win === null) createWindow();
+});
+
+app.on('will-quit', () => {
+  globalShortcut.unregsiterAll();
+});
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
---
+++ src/preload.js
@@ -0,0 +1,186 @@
+// All of the Node.js APIs are available in the preload process.
+// It has the same sandbox as a Chrome extension.
+const gkm = require('gkm');
+const { Howl } = require('howler');
+const glob = require('glob');
+const { shell } = require('electron');
+
+const MV_SET_LS_ID = 'mechvibes-saved-set';
+const MV_VOL_LS_ID = 'mechvibes-saved-volume';
+
+let current_set = null;
+let sets = [];
+let enabled = true;
+let current_key_down = null;
+
+// ==================================================
+// ==================================================
+// ==================================================
+// load all set
+async function loadSets(status_display_elem) {
+  status_display_elem.innerHTML = 'Loading...';
+  sets = [];
+  const folders = await glob.sync('./src/audio/**/');
+  folders.shift();
+  const _sets = folders.map(async folder => {
+    const folder_name = folder.split('/')[3];
+    const config_file = `./audio/${folder_name}/config`;
+    const { set_name, keys, sound_file } = require(config_file);
+    const sound_path = `./audio/${folder_name}/${sound_file}`;
+
+    sound_data = new Howl({
+      src: [sound_path],
+      sprite: keys,
+    });
+
+    const set_data = {
+      set_id: folder_name,
+      set_name,
+      sound: sound_data,
+    };
+
+    sound_data.once('load', function() {
+      set_data._loaded = true;
+      if (isAllSetsLoaded()) {
+        status_display_elem.innerHTML = 'Mechvibes.';
+      }
+    });
+
+    sets.push(set_data);
+  });
+
+  return await Promise.all(_sets);
+}
+
+// ==================================================
+// ==================================================
+// ==================================================
+// check if all sets loaded
+function isAllSetsLoaded() {
+  return sets.every(set => set._loaded);
+}
+
+// ==================================================
+// ==================================================
+// ==================================================
+// get set by id,
+// if id is null,
+// get saved set
+function getSet(set_id = null) {
+  if (!set_id) {
+    if (localStorage.getItem(MV_SET_LS_ID)) {
+      set_id = localStorage.getItem(MV_SET_LS_ID);
+    } else {
+      return sets[0];
+    }
+  }
+  localStorage.setItem(MV_SET_LS_ID, set_id);
+  return sets.find(set => set.set_id == set_id);
+}
+
+// ==================================================
+// ==================================================
+// ==================================================
+// transform set to select option list
+function setsToOptions(sets, set_list, onselect) {
+  // get saved set id
+  const selected_set_id = localStorage.getItem(MV_SET_LS_ID);
+  for (let set of sets) {
+    // check if selected
+    const is_selected = selected_set_id == set.set_id;
+    if (is_selected) {
+      // set current set to saved set
+      current_set = set;
+    }
+    // add set to set list
+    const opt = document.createElement('option');
+    opt.text = set.set_name;
+    opt.value = set.set_id;
+    opt.selected = is_selected ? 'selected' : false;
+    set_list.add(opt);
+  }
+  // on select an option
+  // update saved list id
+  set_list.addEventListener('change', e => {
+    const selected_id = e.target.options[e.target.selectedIndex].value;
+    localStorage.setItem(MV_SET_LS_ID, selected_id);
+    current_set = getSet();
+  });
+}
+
+// ==================================================
+// ==================================================
+// ==================================================
+// main
+(function(window, document) {
+  'use strict';
+
+  window.addEventListener('DOMContentLoaded', async () => {
+    const keycode_display = document.getElementById('keycode-display');
+    const set_list = document.getElementById('set-list');
+    const enable_btn = document.getElementById('enable');
+    const volume_value = document.getElementById('volume-value-display');
+    const volume = document.getElementById('volume');
+
+    // a little hack for open link in browser
+    Array.from(document.getElementsByClassName('open-in-browser')).forEach(elem => {
+      elem.addEventListener('click', e => {
+        e.preventDefault();
+        shell.openExternal(e.target.href);
+      });
+    });
+
+    // listen toggle button
+    enable_btn.addEventListener('click', () => {
+      enabled = !enabled;
+      enable_btn.innerHTML = enabled ? 'Pause' : 'Start';
+    });
+
+    // load all sets
+    await loadSets(keycode_display);
+
+    // get last selected set
+    current_set = getSet();
+
+    // transform sets to options list
+    setsToOptions(sets, set_list);
+
+    // display volume value
+    if (localStorage.getItem(MV_VOL_LS_ID)) {
+      volume.value = localStorage.getItem(MV_VOL_LS_ID);
+    }
+    volume_value.innerHTML = volume.value;
+    volume.oninput = function(e) {
+      volume_value.innerHTML = this.value;
+      localStorage.setItem(MV_VOL_LS_ID, this.value);
+    };
+
+    // listen to key press
+    gkm.events.on('key.*', function(key) {
+      // if turned off, play no sound
+      if (!enabled) {
+        return;
+      }
+      // if key released, clear current key
+      if (this.event === 'key.released') {
+        current_key_down = null;
+      }
+      // key pressed, set current key and play sound
+      if (this.event === 'key.pressed') {
+        if (current_key_down == key[0]) {
+          return;
+        }
+        // display current pressed key
+        keycode_display.innerHTML = key[0];
+        // set current pressed key
+        current_key_down = key[0];
+        // get loaded audio object
+        // if object valid, set volume and play sound
+        if (current_set) {
+          current_set.sound.volume(Number(volume.value / 100));
+          current_set.sound.play(current_key_down.toLowerCase());
+        }
+      }
+    });
+  });
+})(window, document);
