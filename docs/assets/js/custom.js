a8a82622621e9e9d73ee22bef9bf60320fa69003
Hải Nguyễn <hainguyen.ts13@gmail.com>
2020-01-06 17:00:10 +0700

    update index

--- docs/assets/js/custom.js
+++ docs/assets/js/custom.js
@@ -103,9 +103,7 @@ jQuery(function($) {
 
       const dl = {
         exe: latest.assets.find(a => a.content_type == 'application/x-msdownload'),
-        zip: latest.assets.find(a => a.content_type == 'application/zip' || a.content_type == 'application/x-zip-compressed'),
         dmg: latest.assets.find(a => a.content_type == 'application/octet-stream'),
-        // ai: latest.assets.find(a => a.content_type == 'application/x-iso9660-appimage'),
         deb: latest.assets.find(a => a.content_type == 'application/vnd.debian.binary-package'),
       };
 
