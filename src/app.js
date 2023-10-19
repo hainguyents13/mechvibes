afff19b5aad7c10dcf088b18f02d54c91f9035ee
Luna Alfien <xxswatfighterxx@gmail.com>
2023-10-18 20:43:58 -0700

    Fix issue with folders without configs causing loadPacks to throw an error

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const Store = require('electron-store');
 const store = new Store();
 const { Howl } = require('howler');
 const { shell, remote, ipcRenderer } = require('electron');
+const fs = require('fs');
 const glob = require('glob');
 const iohook = require('iohook');
 const path = require('path');
@@ -51,52 +52,54 @@ async function loadPacks(status_display_elem, app_body) {
     const config_file = `${folder.replace(/\/$/, '')}/config.json`;
 
     // get pack info and defines data
-    const { name, includes_numpad, sound = '', defines, key_define_type = 'single' } = require(config_file);
+    if(fs.existsSync(config_file)){
+      const { name, includes_numpad, sound = '', defines, key_define_type = 'single' } = require(config_file);
 
-    // pack sound pack data
-    const pack_data = {
-      pack_id: `${is_custom ? 'custom' : 'default'}-${folder_name}`,
-      group: is_custom ? 'Custom' : 'Default',
-      abs_path: folder,
-      key_define_type,
-      name,
-      includes_numpad,
-    };
-
-    // init sound data
-    if (key_define_type == 'single') {
-      // define sound path
-      const sound_path = `${folder}${sound}`;
-      const sound_data = new Howl({ src: [sound_path], sprite: keycodesRemap(defines) });
-      Object.assign(pack_data, { sound: sound_data });
-      all_sound_files[pack_data.pack_id] = false;
-      // event when sound loaded
-      sound_data.once('load', function () {
-        all_sound_files[pack_data.pack_id] = true;
-        checkIfAllSoundLoaded(status_display_elem, app_body);
-      });
-    } else {
-      const sound_data = {};
-      Object.keys(defines).map((kc) => {
-        if (defines[kc]) {
-          // define sound path
-          const sound_path = `${folder}${defines[kc]}`;
-          sound_data[kc] = new Howl({ src: [sound_path] });
-          all_sound_files[`${pack_data.pack_id}-${kc}`] = false;
-          // event when sound_data loaded
-          sound_data[kc].once('load', function () {
-            all_sound_files[`${pack_data.pack_id}-${kc}`] = true;
-            checkIfAllSoundLoaded(status_display_elem, app_body);
-          });
+      // pack sound pack data
+      const pack_data = {
+        pack_id: `${is_custom ? 'custom' : 'default'}-${folder_name}`,
+        group: is_custom ? 'Custom' : 'Default',
+        abs_path: folder,
+        key_define_type,
+        name,
+        includes_numpad,
+      };
+  
+      // init sound data
+      if (key_define_type == 'single') {
+        // define sound path
+        const sound_path = `${folder}${sound}`;
+        const sound_data = new Howl({ src: [sound_path], sprite: keycodesRemap(defines) });
+        Object.assign(pack_data, { sound: sound_data });
+        all_sound_files[pack_data.pack_id] = false;
+        // event when sound loaded
+        sound_data.once('load', function () {
+          all_sound_files[pack_data.pack_id] = true;
+          checkIfAllSoundLoaded(status_display_elem, app_body);
+        });
+      } else {
+        const sound_data = {};
+        Object.keys(defines).map((kc) => {
+          if (defines[kc]) {
+            // define sound path
+            const sound_path = `${folder}${defines[kc]}`;
+            sound_data[kc] = new Howl({ src: [sound_path] });
+            all_sound_files[`${pack_data.pack_id}-${kc}`] = false;
+            // event when sound_data loaded
+            sound_data[kc].once('load', function () {
+              all_sound_files[`${pack_data.pack_id}-${kc}`] = true;
+              checkIfAllSoundLoaded(status_display_elem, app_body);
+            });
+          }
+        });
+        if (Object.keys(sound_data).length) {
+          Object.assign(pack_data, { sound: keycodesRemap(sound_data) });
         }
-      });
-      if (Object.keys(sound_data).length) {
-        Object.assign(pack_data, { sound: keycodesRemap(sound_data) });
       }
+  
+      // push pack data to pack list
+      packs.push(pack_data);
     }
-
-    // push pack data to pack list
-    packs.push(pack_data);
   });
 
   // end load
